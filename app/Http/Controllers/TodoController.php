c0f8a4f74316e2b9bdaa5e6c833d408aef5a03de
badrman <badr.bechtioui@gmail.com>
2022-07-07 21:02:29 +0100

    the one post in db

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\todo;
+use Illuminate\Http\Request;
+
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $td = new todo();
+        $td->body=$request->input('todobody');
+        $td->save();
+        session()->flash('succes', 'To do Saved With Succes');
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,17 @@
 @extends('layout.master')
 
 @section('content')
-<div class="container">
-    <h1>Our todo App</h1>
+<div class="container my-3">
+    @if (session()->has('succes'))
+    <div class="alert alert-success alert-dismissible">To do Saved With Succes</div>
+    @endif
+    <form action="{{url('/done')}}" method="post">
+     {{ csrf_field() }}
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Add a new todo" aria-label="Recipient's username" aria-describedby="button-addon2" name="todobody">
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Add</button>
+          </div>
+
+    </form>
 </div>
 @endsection
